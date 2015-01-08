1f2153db112d8d3f07a76d690ddefd6ffa25d5e0
Chad Wescott <cwescott@coopervision.com>
2015-01-08 13:51:41 -0500

    Example of a data grid that captures the item that is clicked on.

---
+++ CooperVision.Wpf.Sample/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace CooperVision.Wpf.Sample
+{
+    public class Customer
+    {
+        [DisplayName(@"First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName(@"Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName(@"Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
